4b5390afe1cdece79ee33115588f8283f0a64de7
Brian Hu <blp.hu5@gmail.com>
2021-01-16 03:42:44 -0500

    clean up app.py

--- app.py
+++ app.py
@@ -9,17 +9,5 @@ app.layout = html.Div(
     html.H1(children="Hello world")
 )
 
-'''
-def get_request(province):
-    PROVINCES = ["AB", "BC", "MB", "NB", "NL", "NT", "NS", "NU", "ON", "PE", "QC", "SK", "YT"]
-    URL = f"https://api.opencovid.ca/summary?loc={province}"
-    response = requests.request("GET", URL)
-    json_data = json.loads(response.text)
-    return json_data
-
-def main():
-    print(type(get_request("ON")))
-
-'''
 if __name__ == "__main__":
     app.run_server(debug=True)
