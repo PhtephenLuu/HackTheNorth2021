e0d566b7ad0bd0a01e81e2bcca607c6277fa4487
Stephen Luu <61605603+PhtephenLuu@users.noreply.github.com>
2021-01-15 23:56:19 -0500

    Create requests function

---
+++ graph.py
@@ -0,0 +1,14 @@
+import requests 
+
+
+def reqProv(prov = "ON"):
+    URL = f"https://api.opencovid.ca/summary?loc={prov}"
+    response = requests.request("GET", URL)
+    print(response.text)
+
+
+def main():
+    reqProv()
+
+if __name__ == "__main__":
+    main()
