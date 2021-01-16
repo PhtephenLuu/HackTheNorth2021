23f12f6da8ce2f46da94840d649a272e67a57256
Brian Hu <blp.hu5@gmail.com>
2021-01-16 03:20:16 -0500

    improve datetime handling

--- parse.py
+++ parse.py
@@ -19,19 +19,28 @@ def prompt():
 
 
 def get_weekly():
+    dates = {}
     today = datetime.now()
     yesterday = datetime.now() - timedelta(7)
-    return (today.strftime('%d-%m-%Y'), yesterday.strftime('%d-%m-%Y'))
+    dates['today'] = today.strftime('%d-%m-%Y')
+    dates['yesterday'] = yesterday.strftime('%d-%m-%Y')
+    return dates
 
 def get_monthly():
+    dates = {}
     today = datetime.now()
     yesterday = datetime.now() - timedelta(30)
-    return (today.strftime('%d-%m-%Y'), yesterday.strftime('%d-%m-%Y'))
+    dates['today'] = today.strftime('%d-%m-%Y')
+    dates['yesterday'] = yesterday.strftime('%d-%m-%Y')
+    return dates
 
 def get_yearly():
+    dates = {}
     today = datetime.now()
     yesterday = datetime.now() - timedelta(365)
-    return (today.strftime('%d-%m-%Y'), yesterday.strftime('%d-%m-%Y'))
+    dates['today'] = today.strftime('%d-%m-%Y')
+    dates['yesterday'] = yesterday.strftime('%d-%m-%Y')
+    return dates
 
 def calculate_date(user_choice):
     if user_choice.lower() == "weekly":
@@ -46,13 +55,14 @@ def calculate_date(user_choice):
 
 def get_request(province, dates, stats):
     #dates = calculate_date()
-    URL = f"https://api.opencovid.ca/timeseries?stat={stats}&loc={province}&before={dates[0]}&after={dates[1]}"
+    before_date = dates['yesterday']
+    after_date = dates['today']
+    URL = f"https://api.opencovid.ca/timeseries?stat={stats}&loc={province}&before={before_date}&after={after_date}"
     # DD - MM - YYYY
     response = requests.request("GET", URL)
     json_data = json.loads(response.text)
 
-    n = 4
-    with open(f"sample_outputs/output{n}_{province}_{dates[0]}to{dates[1]}_{stats}.json", "w") as f:
+    with open(f"sample_outputs/{province}_{before_date}to{after_date}_{stats}.json", "w") as f:
         json.dump(json_data, f, indent=4)
     return json_data
 
@@ -63,7 +73,7 @@ def main():
     time_view = result['time_view'] # ex: Weekly
     stats = result['stats'] # ex: cases
 
-    dates = calculate_date(time_view)
+    dates = calculate_date(time_view) # returns dictionary with today/yesterday as keys
 
     get_request(province, dates, stats)
     print("Success")
