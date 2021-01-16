be48b6e3c52ae2e3d57b3a7fb1386a18d924cf17
Stephen Luu <61605603+PhtephenLuu@users.noreply.github.com>
2021-01-16 01:35:55 -0500

    add json functionality

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import requests
+import json
 import dash
 import pandas 
 
 def reqProv(prov = "ON"):
     URL = f"https://api.opencovid.ca/summary?loc={prov}"
     response = requests.request("GET", URL)
+    data = json.loads(response.text)
     print(response.text)
+    print(data["summary"])
 
+def reqTimeSeries(prov = "ON"):
+    pass
 
 def main():
     reqProv()
