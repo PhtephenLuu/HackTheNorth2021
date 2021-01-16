70bbc5bc924fc93e1b596efcbadf6ea39f232513
Brian Hu <blp.hu5@gmail.com>
2021-01-16 02:29:38 -0500

    add dash components

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
-import requests
 import dash
-import pandas 
+import dash_core_components as dcc
+import dash_html_components as html
 
-def reqProv(prov = "ON"):
-    URL = f"https://api.opencovid.ca/summary?loc={prov}"
+import json
+import requests
+
+app = dash.Dash()
+
+app.layout = html.Div(
+    html.H1(children="Hello world")
+)
+
+'''
+def get_request(province):
+    PROVINCES = ["AB", "BC", "MB", "NB", "NL", "NT", "NS", "NU", "ON", "PE", "QC", "SK", "YT"]
+    URL = f"https://api.opencovid.ca/summary?loc={province}"
     response = requests.request("GET", URL)
-    print(response.text)
-
+    json_data = json.loads(response.text)
+    return json_data
 
 def main():
-    reqProv()
+    print(type(get_request("ON")))
 
+'''
 if __name__ == "__main__":
-    main()
+    app.run_server(debug=True)
