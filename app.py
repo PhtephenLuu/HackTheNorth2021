3dfe0eb714e77a1941b9a30caf519d611cdd1636
Brian Hu <blp.hu5@gmail.com>
2021-01-16 03:51:25 -0500

    add dropdown menu in app.py

--- app.py
+++ app.py
@@ -2,12 +2,38 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 
-
 app = dash.Dash()
 
-app.layout = html.Div(
-    html.H1(children="Hello world")
-)
+app.layout = html.Div([
+    dcc.Dropdown(
+        id='province-dropdown',
+        # AB", "BC", "MB", "NB", "NL", "NT", "NS", "NU", "ON", "PE", "QC", "SK", "YT
+        options=[
+            {'label': 'Alberta', 'value': 'AB'},
+            {'label': 'British Columbia', 'value': 'BC'},
+            {'label': 'Manitoba', 'value': 'MB'},
+            {'label': 'New Brunswick', 'value': 'NB'},
+            {'label': 'Northwest Territories', 'value': 'NT'},
+            {'label': 'Nova Scotia', 'value': 'NS'},
+            {'label': 'Newfoundland and Labrador', 'value': 'NL'},
+            {'label': 'Nunavut', 'value': 'NU'},
+            {'label': 'Ontario', 'value': 'ON'},
+            {'label': 'Prince Edward Island', 'value': 'PE'},
+            {'label': 'Quebec City', 'value': 'QC'},
+            {'label': 'Saskatchewan', 'value': 'SK'},
+            {'label': 'Yukon', 'value': 'YT'}
+        ],
+        placeholder="Select a province",
+    ),
+    html.Div(id='dd-output-container')
+])
+
+@app.callback(
+    dash.dependencies.Output('dd-output-container', 'children'),
+    [dash.dependencies.Input('province-dropdown', 'value')])
+
+def update_output(value):
+    return 'You have selected "{}"'.format(value)
 
 if __name__ == "__main__":
     app.run_server(debug=True)
